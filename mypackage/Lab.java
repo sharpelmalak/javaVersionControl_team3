94b222fc96a63e39c919e81040b615ccd6838330
SharpelMalak <sharbelmalak@gmail.com>
2024-07-26 14:52:04 +0300

    delete task 1

--- mypackage/Lab.java
+++ mypackage/Lab.java
@@ -5,13 +5,6 @@ public class Lab {
 		// taskOne();
 		taskThree();
 	}
-
-	public static void taskOne() {
-		TemperatureConvert Tc = new TemperatureConvert();
-		System.out.println("Temp in C is " + 15);
-		System.out.println("Temp in F is " + Tc.apply(15f));
-	}
-
 	public static void taskThree() {
 		double[] co = { 1.0, -2.0, 1.0 };
 		QuadraticEquation qe = new QuadraticEquation();
