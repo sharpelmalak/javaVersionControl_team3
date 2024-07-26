6e503b2667bdc72ab8a01b4cc35d2deb55241eff
Mohamed Saber <mohamedsaber.developer@gmail.com>
2024-07-26 14:49:38 +0300

    change from Function to UnaryOperator since the input and return type are equal

--- mypackage/TemperatureConvert.java
+++ mypackage/TemperatureConvert.java
@@ -1,10 +1,12 @@
 package mypackage;
 
-import java.util.function.Function;
+import java.util.function.UnaryOperator;
 
-public class TemperatureConvert implements Function<Float, Float> {
+public class TemperatureConvert implements UnaryOperator<Float> {
+
 	@Override
 	public Float apply(Float t) {
 		return 1.8f * t + 32;
 	}
+
 }
